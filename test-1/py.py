215da8b2bf9b6d14842937af7dc7ed9a3db2365a
Jacky <tanzhijie792@qq.com>
2021-03-06 23:03:21 +0800

    commit 2

--- test-1/py.py
+++ test-1/py.py
@@ -1,5 +1,5 @@
 import datetime
-print("hello pycharm!")
-print ("push github")
-print ("push again")
+print("hello pycharm!",end="!!!")
+print ("push github",end="|||")
+print ("push again",end="")
 print ("Hello Github")
